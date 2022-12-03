6a0b97d1bfdad21f25ee74ff358776de536613ba
rushdihijab <103061146+rushdihijab@users.noreply.github.com>
2022-12-03 18:28:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 const cors = require('cors');
+app.use(cors());
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
